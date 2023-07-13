9ce50b35df5573017cc3400a6614093544f935be
ivenckunas <ignas.venckunas@gmail.com>
2023-07-13 12:27:40 +0300

    brand features

--- vite-project/src/Components/BrandId/BrandId.jsx
+++ vite-project/src/Components/BrandId/BrandId.jsx
@@ -1,25 +1,45 @@
 import './brandid.css';
+import projectsArr from '../../Data/projectsArr';
+import MainContext from '../../Context/MainContext';
+import {useContext, useState} from 'react';
+
 function BrandId() {
+	const {project} = useContext(MainContext);
+	const currentBrand = projectsArr[project];
+	const [feature, setFeature] = useState();
+	const activeFeature = currentBrand.features[feature];
+
 	return (
 		<div className='brand-container'>
-			<div className='brand-img'>
-				<img
-					src='https://images.pexels.com/photos/10874552/pexels-photo-10874552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-					alt=''
-				/>
-			</div>
+			{!activeFeature && (
+				<div className='brand-img'>
+					<img
+						src={currentBrand.image}
+						alt=''
+					/>
+				</div>
+			)}
+			{activeFeature && (
+				<div className='active-brand-img'>
+					<img
+						src={activeFeature.image}
+						alt=''
+					/>
+				</div>
+			)}
 			<div className='brand-desc'>
-				<h2 className='name'>Hirundo farm</h2>
-				<p className='desc'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+				<h2 className='name'>{currentBrand.name}</h2>
+				<p className='desc'>{currentBrand.desc}</p>
 				<ul>
-					<li className='active'>Logo</li>
-					<li>Colors</li>
-					<li>Typography</li>
-					<li>Iconography</li>
-					<li>Imagery</li>
-					<li>Personality</li>
-					<li>Voice</li>
-					<li>Web</li>
+					{currentBrand.features.map((feature, id) => (
+						<li
+							key={id}
+							onClick={() => setFeature(id)}
+							className={`${feature === activeFeature && 'active'}`}
+						>
+							{feature.name}
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
--- vite-project/src/Data/projectsArr.jsx
+++ vite-project/src/Data/projectsArr.jsx
@@ -1,8 +1,43 @@
 const projectsArr = [
 	{
-		name: 'wwwwwwwwwwwwwwwwww',
+		name: 'Hirundo farm',
 		type: 'brand',
 		image: 'https://images.pexels.com/photos/2457284/pexels-photo-2457284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+		desc: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.',
+		features: [
+			{
+				name: 'Logo',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+			{
+				name: 'Colors',
+				image: 'https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg',
+			},
+			{
+				name: 'Typography',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+			{
+				name: 'Iconography',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+			{
+				name: 'Imagery',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+			{
+				name: 'Personality',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+			{
+				name: 'Voice',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+			{
+				name: 'Web',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+			},
+		],
 	},
 	{
 		name: 'sssssssssssss',
